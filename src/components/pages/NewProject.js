492c6ec48e544d35d6b926a0a93cb3eff3fdc886
Joabe-Albino <joabefelipealbino@gmail.com>
2026-02-01 07:12:59 -0300

    método de criação do forms add

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,15 +1,44 @@
-import ProjectForm from '../project/ProjectForm'
+import { useNavigate } from "react-router-dom";
 
-import styles from './NewProject.module.css'
+import ProjectForm from "../project/ProjectForm";
 
-function NewProject(){
-    return (
-        <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adiconar os serviços</p>
-            <ProjectForm btnText={"Criar Projeto"}/>
-        </div>
-    )
+import styles from "./NewProject.module.css";
+
+function NewProject() {
+  const navigate = useNavigate();
+
+  function createProject(project) {
+    project.costs = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(project), //Retorna o nosso projeto pra conseguir enviar
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        //redirect para projects
+        navigate("/projects", {
+          state: {
+            message: "Projeto criado com sucesso",
+          },
+        });
+
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adiconar os serviços</p>
+      <ProjectForm handleSubmit={createProject} btnText={"Criar Projeto"} />
+    </div>
+  );
 }
 
-export default NewProject
+export default NewProject;
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -6,8 +6,9 @@ import Input from "../form/Input";
 import Select from "../form/Select";
 import ButtonSubmit from "../form/ButtonSubmit";
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
     fetch("http://localhost:5000/categories", {
@@ -17,29 +18,57 @@ function ProjectForm({ btnText }) {
       },
     })
       .then((response) => response.json())
-      .then((data) => {setCategories(data)})
+      .then((data) => {
+        setCategories(data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
+  const submit = (event) => {
+    event.preventDefault();
+    handleSubmit(project)
+    // console.log(project);
+  }
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value }); //Vai continuar com a estrutura do projeto e indepente de qual for o nosos input name||budget vai pegar seus respectivos values
+  }
+
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type={"text"}
         placeholder={"Insira o nome do projeto"}
         name={"name"}
         text={"Nome do projeto"}
+        handleOnChange={handleChange}
+        value={project.name ? project.name : '' }
       />
       <Input
         type={"number"}
         text={"Orçamento do projeto"}
         placeholder={"Insira o orçamento do projeto"}
         name={"budget"}
+        handleOnChange={handleChange}
+        value={project.budget ? project.budget : '' }
       />
 
       <Select
         text={"Selecione a categoria"}
         name={"category_id"}
         options={categories}
+        handleOnChange={handleCategory}
+        value={project.category ? project.category.id : '' } //Verificamos se tem o id se não tiver ele retorna o id vazio, fazemos isso pela nossa primeira option de "selecione uma opção"
       />
 
       <ButtonSubmit text={btnText} />
