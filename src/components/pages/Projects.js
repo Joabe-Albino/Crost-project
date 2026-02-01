38f0aea27c2e2299340d6ae4bed255e7b289fe21
Joabe-Albino <joabefelipealbino@gmail.com>
2026-02-01 08:54:23 -0300

    Criado component message

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,7 +1,18 @@
+import Message from "../layout/Message"
+
+import {useLocation} from 'react-router-dom'
+
 function Projects(){
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
     return (
         <div>
-            <h1>Projetos</h1>
+            <h1>Meus Projetos</h1>
+            {message && <Message msg={message} type="success"/>}
         </div>
     )
 }
